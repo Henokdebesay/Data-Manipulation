d66b7edfba40eaa5116a4b7ba080c85813a5a21b
Henok Debesay <henokdebesay@gmail.com>
2024-01-12 13:53:01 -0800

    fixes some bug on part 1

--- app.js
+++ app.js
@@ -28,51 +28,52 @@ console.log (`The four numbers are valid ccording to the provided criteria: ${is
 // Part 2: Practical Math
 
 
-// const tripDistance = 1500;
-// const fuelBudget = 175;
-// const costFuel_perGalllon = 3;
-// let gallon_fiftyfivePerHour = 30;
-// let gallon_sixtyPerHour = 28;
-// let gallon_seventyFivePerHour = 23;
+const tripDistance = 1500;
+const fuelBudget = 175;
+const costFuel_perGalllon = 3;
+let gallon_fiftyfivePerHour = 30;
+let gallon_sixtyPerHour = 28;
+let gallon_seventyFivePerHour = 23;
 
-// const gallonsForTrip = fuelBudget / costFuel_perGalllon;
+const gallonsForTrip = fuelBudget / costFuel_perGalllon;
 
-//  let gallons_used= "";
+ let gallons_used= "";
 
-// function CalculateGallons(n){
+function CalculateGallons(n){
 
-//     if (n === 30){
-//          gallons_used = tripDistance / n;
-//     } else if (n === 38){
-//       gallons_used = tripDistance / n;
-//     } else if (n === 23){
-//      gallons_used = tripDistance / n;
-//     } else{
-//       console.log("Enter another number");
-//     }
-// }
+    if (n === 30){
+         gallons_used = tripDistance / n;
+    } else if (n === 38){
+      gallons_used = tripDistance / n;
+    } else if (n === 23){
+     gallons_used = tripDistance / n;
+    } else{
+      console.log("Enter another number");
+    }
+}
 
-// // Enter "n" which is the number of miles/hr and it will show you how many gallons you would use during the trip
-// let hoursOfTravel = "";
-// function hours(n){
-//    if (n === 55){
-//     hoursOfTravel = Math.round(tripDistance / n);
-//    }else if(n === 60){
-//     hoursOfTravel = Math.round(tripDistance / n);
-//    }else if(n === 75){
-//     hoursOfTravel = Math.round(tripDistance / n);
-//    }else{
-//     console.log("Enter another number");
-//    }
-// }
+// Enter "n" which is the number of miles/hr and it will show you how many gallons you would use during the trip
+let hoursOfTravel = "";
+function hours(n){
+   if (n === 55){
+    hoursOfTravel = Math.round(tripDistance / n);
+   }else if(n === 60){
+    hoursOfTravel = Math.round(tripDistance / n);
+   }else if(n === 75){
+    hoursOfTravel = Math.round(tripDistance / n);
+   }else{
+    console.log("Enter another number");
+   }
+}
 
 
-// //Enter the speed rate you will be going at,
-// hours(55);
-// //Enter the number of miles per gallon you will use up at that speed
-// CalculateGallons(30);
+//Enter the speed rate you will be going at,
+hours(55);
+//Enter the number of miles per gallon you will use up at that speed
+CalculateGallons(30);
+//
 
-// console.log(`At that speed, you will use ${gallons_used} gallons for the entire trip and It will take you ${hoursOfTravel} hours`);
+console.log(`At that speed, you will use ${gallons_used} gallons for the entire trip and It will take you ${hoursOfTravel} hours`);
 
 
 
